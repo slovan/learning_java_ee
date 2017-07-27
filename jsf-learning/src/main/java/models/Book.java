9e0a86585beff178155391618fdec67151b8df76
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2017-07-27 13:27:27 +0200

    add Book entity

---
+++ jsf-learning/src/main/java/models/Book.java
@@ -0,0 +1,62 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@NamedQuery(name = "findAllBooks", query = "SELECT b FROM Book b ORDER BY b.title DESC")
+public class Book {
+	@Id @GeneratedValue
+	private Long id;
+	@NotNull @Size(min = 4, max = 50)
+	@Column(nullable = false)
+	private String title;
+	private Float price;
+	@Column(length = 2000)
+	private String description;
+	private Integer nbOfPage;
+	private Boolean illustrations;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getNbOfPage() {
+		return nbOfPage;
+	}
+	public void setNbOfPage(Integer nbOfPage) {
+		this.nbOfPage = nbOfPage;
+	}
+	public Boolean getIllustrations() {
+		return illustrations;
+	}
+	public void setIllustrations(Boolean illustrations) {
+		this.illustrations = illustrations;
+	}
+}
