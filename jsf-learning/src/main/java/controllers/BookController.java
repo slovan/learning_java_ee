d22048b6e610126ca5b2780dd9ae992c403d195c
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2017-07-31 15:01:01 +0200

    add warning/error messages in backing bean

--- jsf-learning/src/main/java/controllers/BookController.java
+++ jsf-learning/src/main/java/controllers/BookController.java
@@ -18,10 +18,25 @@ public class BookController {
 	private Book book = new Book();
 
 	public String doCreateBook() {
-		bookEJB.createBook(book);
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Book created",
-				"The book" + book.getTitle() + " has been created with id=" + book.getId()));
-		return "newBook.xhtml";
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		if (book.getTitle() == null || "".equals(book.getTitle())) {
+			ctx.addMessage("bookForm:title", new FacesMessage(FacesMessage.SEVERITY_WARN, "Wrong title",
+					"You should enter a title for the book"));
+		}
+
+		if (ctx.getMessageList().size() != 0)
+			return null;
+
+		try {
+			bookEJB.createBook(book);
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,
+					"Book created", "The book" + book.getTitle() + " has been created with id=" + book.getId()));
+			return "newBook.xhtml";
+		} catch (Exception e) {
+			ctx.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Book hasn't been created", e.getMessage()));
+		}
+		return null;
 	}
 
 	public void doFindBookById() {
